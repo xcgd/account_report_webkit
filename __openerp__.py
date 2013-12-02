64d6e8518ca9794583ab5e570ab28e834fe68fab
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2013-12-02 18:08:29 +0100

    Version 1.1

--- __openerp__.py
+++ __openerp__.py
@@ -114,7 +114,7 @@ Other improvements:
  - Filter by ledger type.
  - Group by analytic codes.
     ''',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'XCG',
     'license': 'AGPL-3',
     'category': 'Finance',

dfe0404278a8cf3c45de34737fc0bfc5057d5975
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-10-07 12:09:21 +0200

    Bumped to version 2.0.1 (dev)

--- __openerp__.py
+++ __openerp__.py
@@ -108,7 +108,7 @@ Other improvements:
  - Filter by ledger type.
  - Group by analytic codes.
     ''',
-    'version': '2.0.1',
+    'version': '2.0.2',
     'author': 'XCG Consulting',
     'license': 'AGPL-3',
     'category': 'Finance',
